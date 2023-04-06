96dbf604ad4410fbb3e375577bfd35649a05bd8f
Ilya <ilyaushenin@gmail.com>
2023-04-06 11:01:35 +0300

    ViewModel-LiveData

--- app/src/main/java/com/birdushenin/assemblingthings/presentation/MainViewModel.kt
+++ app/src/main/java/com/birdushenin/assemblingthings/presentation/MainViewModel.kt
@@ -1,6 +1,5 @@
 package com.birdushenin.assemblingthings.presentation
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.birdushenin.assemblingthings.data.ShopListRepositoryImpl
 import com.birdushenin.assemblingthings.domain.DeleteShopItemUseCase
@@ -16,22 +15,16 @@ class MainViewModel: ViewModel() {
     private val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
     private val editShopItemUseCase = EditShopItemUseCase(repository)
 
-    val shopList = MutableLiveData<List<ShopItem>>()
+    val shopList = getShopListUseCase.getShopList()
 
-    fun getShopList() {
-        val list = getShopListUseCase.getShopList()
-        shopList.value = list
-    }
 
     fun deleteShopItem(shopItem: ShopItem) {
         deleteShopItemUseCase.deleteShopItem(shopItem)
-        getShopList()
     }
 
     fun changeEnableState(shopItem: ShopItem) {
         val newItem = shopItem.copy(enabled = !shopItem.enabled)
         editShopItemUseCase.editShopItem(newItem)
-        getShopList()
     }
 
 }
