c36f1514c4610e033988dc691831b4dde1fc86aa
Ilya <ilyaushenin@gmail.com>
2023-04-13 15:23:06 +0300

    Completed-UI

---
+++ app/src/main/java/com/birdushenin/assemblingthings/presentation/ShopItemActivity.kt
@@ -0,0 +1,12 @@
+package com.birdushenin.assemblingthings.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.birdushenin.assemblingthings.R
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
